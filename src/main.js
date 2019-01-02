108996360bf98110a0a5fd378ee65cfeb8106ddf
zj <3280205466@qq.com>
2019-01-02 14:53:16 +0800

    渲染home组件

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
+import Http from '@/plugins/http'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/index.css' 
 import router from './router'
 
 
 Vue.config.productionTip = false
+ 
+Vue.use(Http)
+ 
 Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
